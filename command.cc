4078c1881dd1b038bd6938c54005f754a78a2714
Shafay Haq <haqs@purdue.edu>
2018-02-12 01:27:29 -0500

    Commit

--- command.cc
+++ command.cc
@@ -149,8 +149,7 @@ void Command::execute() {
 	    char *argv[_simpleCommands[i]->_arguments.size() + 1];
 	    size_t k;
 	      for(k =0; k < _simpleCommands[i]->_arguments.size(); k++){
-		*argv =  (char *) _simpleCommands[i]->_arguments[k]->c_str();
-		*argv++;
+		*argv[k] =  (char *) _simpleCommands[i]->_arguments[k]->c_str();
 	      }
 		argv[k] = NULL;
 		execvp(_simpleCommands[i]->_arguments[0]->c_str(), argv);

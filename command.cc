3a8c7bf85a251c2f549869436b9e2c425a9116e0
Shafay Haq <haqs@purdue.edu>
2018-02-12 00:28:53 -0500

    Commit

--- command.cc
+++ command.cc
@@ -143,7 +143,7 @@ void Command::execute() {
 	  close(fdout);
 	  ret = fork();
 	  if(ret == 0){
-	    printf("Yass jesus %d", ret);
+	    //printf("Yass jesus %d", ret);
 	    char *argv[_simpleCommands[i]->_arguments.size() + 1];
 	    int j =0;
 	      for(uint k =0; i < _simpleCommands[i]->_arguments.size(); k++){
@@ -156,7 +156,7 @@ void Command::execute() {
 	    perror("fork");
 	    exit(1);
 	  }else{
-	    printf("help me jesus!! %d", ret);
+	    //printf("help me jesus!! %d", ret);
 	  }
 	}
 	

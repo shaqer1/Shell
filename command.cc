1d841d9e4494adda462958ef106905729ebdecf5
Shafay Haq <haqs@purdue.edu>
2018-02-15 00:58:56 -0500

    Commit

--- command.cc
+++ command.cc
@@ -22,6 +22,11 @@
 #include "shell.hh"
 #include <stdlib.h>
 
+extern "C" void disp( int sig )
+{
+	fprintf( stderr, "\nsig:%d      Ouch!\n", sig);
+}
+
 Command::Command() {
     // Initialize a new vector of Simple Commands
     _simpleCommands = std::vector<SimpleCommand *>();

4131c179fb77d5ef29b25ab4128a0b34a7b96c32
Shafay Haq <haqs@purdue.edu>
2018-02-15 01:48:47 -0500

    Commit

--- shell.cc
+++ shell.cc
@@ -29,32 +29,29 @@ void Shell::prompt() {
       printf("myshell>");
     }
     fflush(stdout);
-  }/*
-  printf("myshell>");
-  fflush(stdout);*/
+  }
 }
 
 int main() {
 
   struct sigaction sa;
-    sa.sa_handler = disp;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
+  sa.sa_handler = disp;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if(sigaction(SIGINT, &sa, NULL)){
+    perror("sigaction");
+    exit(2);
+  }
 
-    if(sigaction(SIGINT, &sa, NULL)){
-        perror("sigaction");
-	exit(2);
-    }
+  struct sigaction sa2;
+  sa2.sa_handler = killZombies;
+  sigemptyset(&sa2.sa_mask);
+  sa2.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa2, NULL)) {
+    perror("sigactionZombie");
+    exit(-1);
+  }
 
-    struct sigaction sa2;
-    sa2.sa_handler = killZombies;
-    sigemptyset(&sa2.sa_mask);
-    sa2.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa2, NULL)) {
-      perror("sigactionZombie");
-      exit(-1);
-    }
-  
   Shell::prompt();
   yyparse();
 }

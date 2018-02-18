b66abdc2b552c89e37828f74393d0a6f445a29f6
Shafay Haq <haqs@purdue.edu>
2018-02-18 00:16:50 -0500

    Commit

--- simpleCommand.cc
+++ simpleCommand.cc
@@ -26,7 +26,7 @@ void SimpleCommand::insertArgument( std::string * argument ) {
     while (i != argument->size()) {
       if (argument->at(i) == '$') {
         i = argument->find('}');
-        std::string varName = argument->substr(argument->find('{') +1, i - argument->find('{') +1);
+        std::string varName = argument->substr(argument->find('{') +1, i - (argument->find('{') +1);
         std::string var (getenv(varName.c_str()));
         argument->assign(argument->substr(0,argument->find('{')) + var + 
           argument->substr(argument->find("}" +1)));

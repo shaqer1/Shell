7920daeec76b866e69ff82300f98bbf276041941
Shafay Haq <haqs@purdue.edu>
2018-02-19 17:11:45 -0500

    Commit

--- command.cc
+++ command.cc
@@ -112,12 +112,12 @@ for (;;) {
     }
  */
 /*extern char ** environ;
-extern void yypush_buffer_state (YY_BUFFER_STATE new_buffer );
 extern void yyparse();*/
 extern FILE * yyin;
 extern char * yytext;
 extern int YY_BUF_SIZE;
 extern int yy_create_buffer (FILE *file,int size  );
+extern void yypush_buffer_state (int new_buffer );
 
 void Command::execute() {
     // Don't do anything if there are no simple commands

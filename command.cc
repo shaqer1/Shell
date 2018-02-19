ab57a4e8e7517baf29455b80fe878e9d8ffcad4c
Shafay Haq <haqs@purdue.edu>
2018-02-19 11:24:51 -0500

    Commit

--- command.cc
+++ command.cc
@@ -129,10 +129,11 @@ void Command::execute() {
       printf("Ambiguous output redirect.\n");
     }
     
-    if (!strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "exit") || !strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "exit1")) {
-      if (strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "exit1")) {
+    if (!strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "exit") 
+        /*|| !strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "exit1")*/) {
+      //if (strcmp(_simpleCommands[0]->_arguments[0]->c_str(), "exit1")) {
         printf("\nGood Bye!!\n\n");
-      }  
+      //}  
       exit(0);
     }
 

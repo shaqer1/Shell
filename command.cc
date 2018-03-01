8e291f2ae6a6cd762a950f03d2390c105b25af7f
Shafay Haq <haqs@purdue.edu>
2018-03-01 00:19:45 -0500

    Commit

--- command.cc
+++ command.cc
@@ -234,7 +234,7 @@ void Command::execute() {
 		argv[k] =  (char *) _simpleCommands[i]->_arguments[k]->c_str();
 	      }
 		argv[k] = NULL;
-        execCode = execvp(_simpleCommands[i]->_arguments[0]->c_str(), argv);
+        myExecCode = execvp(_simpleCommands[i]->_arguments[0]->c_str(), argv);
 		exit(1);
 	  }else if(ret < 0){
 	    perror("fork");

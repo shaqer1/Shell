ddd484ee15c631e5eb53f9ff4457c37f2c9f77c9
Shafay Haq <haqs@purdue.edu>
2018-03-07 22:20:13 -0500

    Commit

--- command.cc
+++ command.cc
@@ -36,14 +36,15 @@
 extern char ** history;
 extern int history_length;
 
-/*void bgHandler(int sig){
-  while(waitpid(-1, NULL, WNOHANG) >0){
+void bgHandler(int sig){
+    int pid
+  while((pid = waitpid(-1, NULL, WNOHANG)) >0){
 	if(_background){
-            printf("[%d] exited\n", getpid());
+            printf("[%d] exited\n", pid);
 
 	}
   }
-}*/
+}
 
 Command::Command() {
     // Initialize a new vector of Simple Commands
@@ -55,7 +56,7 @@ Command::Command() {
     _background = false;
 
 
-        /*struct sigaction sa3;
+        struct sigaction sa3;
         sa3.sa_handler = bgHandler;
         sigemptyset(&sa3.sa_mask);
         sa3.sa_flags = SA_RESTART;
@@ -63,7 +64,7 @@ Command::Command() {
         if ((error = sigaction(SIGCHLD, &sa3, NULL))) {
             perror("child");
             exit(-1);
-        }*/
+        }
 }
 
 void Command::insertSimpleCommand( SimpleCommand * simpleCommand ) {
